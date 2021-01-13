b360de7557e1b20db0ebb83792d2868c1086436e
trmnaoya <trmnaoya@gmail.com>
2021-01-13 23:40:33 +0900

    完成

--- script.js
+++ script.js
@@ -6,15 +6,31 @@ $(function () {
         $('#nav').removeClass('fixed');
       }
     });
-  });
+});
 
 $(function(){
     $('a[href^="#"]').click(function(){
       var speed = 500;
       var href= $(this).attr("href");
       var target = $(href == "#" || href == "" ? 'html' : href);
-      var position = target.offset().top;
+      var position = target.offset().top-130;
       $("html, body").animate({scrollTop:position}, speed, "swing");
       return false;
     });
+});
+
+// ウィンドウを開く
+$( '.js-modal-open' ).each( function() {
+  $( this ).on( 'click', function() {
+       var target = $( this ).data( 'target' );
+       var modal = document.getElementById( target );
+       $( modal ).fadeIn( 300 );
+       return false;
   });
+});
+
+// ウィンドウを閉じる
+$( '.js-modal-close' ).on( 'click', function() {
+ $( '.js-modal' ).fadeOut( 300 );
+ return false;
+});
