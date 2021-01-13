f74a0ee8b551f7b0dd72e148920ea19303b5efc0
trmnaoya <trmnaoya@gmail.com>
2021-01-13 10:41:33 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+$(function () {
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 550) {
+        $('#nav').addClass('fixed');
+      } else {
+        $('#nav').removeClass('fixed');
+      }
+    });
+  });
+
+$(function(){
+    $('a[href^="#"]').click(function(){
+      var speed = 500;
+      var href= $(this).attr("href");
+      var target = $(href == "#" || href == "" ? 'html' : href);
+      var position = target.offset().top;
+      $("html, body").animate({scrollTop:position}, speed, "swing");
+      return false;
+    });
+  });
